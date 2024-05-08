e1bbfb2da85278d818231072f9e66a18fd19762a
Sagar Hedaoo <sagarhedaoo@gmail.com>
2024-05-07 22:36:00 -0400

    Logging setup

--- src/main/java/com/hedaoosagar/WebAPI/WebApiApplication.java
+++ src/main/java/com/hedaoosagar/WebAPI/WebApiApplication.java
@@ -1,5 +1,7 @@
 package com.hedaoosagar.WebAPI;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -7,12 +9,11 @@ import org.springframework.context.ConfigurableApplicationContext;
 @SpringBootApplication
 public class WebApiApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(WebApiApplication.class);
+
 	public static void main(String[] args) {
-
-		ConfigurableApplicationContext context = SpringApplication.run(WebApiApplication.class, args);
-
-		WelcomeMessage welcomeMessage = (WelcomeMessage) context.getBean("welcomeMessage");
-		System.out.println(welcomeMessage);
+		SpringApplication.run(WebApiApplication.class, args);
+		log.info("Application started Successfully!");
 
 	}
 
