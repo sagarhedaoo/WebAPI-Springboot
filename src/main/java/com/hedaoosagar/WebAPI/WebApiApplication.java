3051b64654fdbf004d3f7084cc93d7febf2c3d8e
Sagar Hedaoo <sagarhedaoo@gmail.com>
2024-05-09 20:39:49 -0400

    Integrated HttpClient

--- src/main/java/com/hedaoosagar/WebAPI/WebApiApplication.java
+++ src/main/java/com/hedaoosagar/WebAPI/WebApiApplication.java
@@ -1,13 +1,16 @@
 package com.hedaoosagar.WebAPI;
 
 import com.hedaoosagar.WebAPI.user.User;
-import com.hedaoosagar.WebAPI.user.UserRestClient;
+import com.hedaoosagar.WebAPI.user.UserHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import java.util.List;
 
@@ -23,7 +26,14 @@ public class WebApiApplication {
     }
 
     @Bean
-    CommandLineRunner runner(UserRestClient client) {
+    UserHttpClient userHttpClient() {
+        RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+        HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+        return factory.createClient(UserHttpClient.class);
+    }
+
+    @Bean
+    CommandLineRunner runner(UserHttpClient client) {
         return args -> {
             List<User> users = client.findAll();
             System.out.println(users);
--- src/main/java/com/hedaoosagar/WebAPI/user/UserRestClient.java
+++ src/main/java/com/hedaoosagar/WebAPI/user/UserRestClient.java
@@ -12,8 +12,10 @@ public class UserRestClient {
     private final RestClient restClient;
 
     public UserRestClient(RestClient.Builder builder) {
+
         this.restClient = builder
                 .baseUrl("https://jsonplaceholder.typicode.com/")
+                .defaultHeader("User-Agent", "RestClient")
                 .build();
     }
 
